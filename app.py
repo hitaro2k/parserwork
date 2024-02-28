c26cc7c29d49f5c350a285ec7c4bee8a943b43bb
hitaro2k <vlad.bojenko2005@gmail.com>
2024-02-28 15:26:12 +0300

    ending add hours in news.

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ news ={
         "tech":[],
         "ent":[],
         "other":[]
-    },
-    "ukraine_news":{
-
     }
 }
 
@@ -22,4 +19,4 @@ main_page(news["global_news"]["eu"],
 
 
 
-print(news["global_news"]["us"])
+print(news["global_news"]["other"])
